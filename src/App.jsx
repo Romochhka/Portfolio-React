0696d9e265b416794173bf95cf687521e27c5f1f
Ramzat Ruslanov <ruslanov595@gmail.com>
2024-11-28 14:35:08 +0500

    finished making the Hero.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Tech from "./components/Tech.jsx";
@@ -6,27 +5,22 @@ import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx";
 
 function App() {
-  return <>
-    <div
-        className="fixed -z-10 min-h-screen w-full
-        [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-
-    <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
-      <Navbar/>
-      <Hero/>
-      <Tech/>
-      <Projects/>
-      <Contact/>
-    </main>
-  </>
-=======
-function App() {
   return (
-    <>
-        <div className='text-8xl font-bold'>hello world</div>
-    </>
-  )
->>>>>>> 4921ebcace44a31203642e17919fa75153fee965
+      <>
+        <div
+            className="fixed -z-10 min-h-screen w-full
+        [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+        </div>
+
+        <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+          <Navbar />
+          <Hero />
+          <Tech />
+          <Projects />
+          <Contact />
+        </main>
+      </>
+  );
 }
 
-export default App
+export default App;
